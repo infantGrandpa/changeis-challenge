e7a9760cc7b160550c137a5b8dbc875479b95e41
Brett Abraham <babraham341@gmail.com>
2025-11-16 14:39:49 -0500

    Format DataDisplay.tsx

--- src/components/DataDisplay.tsx
+++ src/components/DataDisplay.tsx
@@ -5,24 +5,26 @@ import Image from "./Image.tsx";
 export default function DataDisplay() {
     const {data, error, isLoading} = useFaker('images', 3);
 
-    return <div className="text-2xl">
-        <p>Data Display</p>
-        {isLoading && <p>Loading...</p>}
-        {error && <p className="text-red-500">Error: {error}</p>}
-        <div className="flex flex-row flex-wrap">
-            {/*
-            We append the index as the version to each image URL to prevent it from being stored in the cache.
-            Since each image is technically coming from the same URL, the browser caches it.
-            Adding version to the url prevents this, allowing us to display multiple images from the same url.
-             */}
-            {data?.map((image: ImageResponse, index: number) => (
-                <Image
-                    key={index}
-                    imageUrl={`${image.url}?v=${index}`}
-                    title={image.title}
-                    altText={image.title}
-                />
-            ))}
+    return (
+        <div className="text-2xl">
+            <p>Data Display</p>
+            {isLoading && <p>Loading...</p>}
+            {error && <p className="text-red-500">Error: {error}</p>}
+            <div className="flex flex-row flex-wrap">
+                {/*
+                We append the index as the version to each image URL to prevent it from being stored in the cache.
+                Since each image is technically coming from the same URL, the browser caches it.
+                Adding version to the url prevents this, allowing us to display multiple images from the same url.
+                 */}
+                {data?.map((image: ImageResponse, index: number) => (
+                    <Image
+                        key={index}
+                        imageUrl={`${image.url}?v=${index}`}
+                        title={image.title}
+                        altText={image.title}
+                    />
+                ))}
+            </div>
         </div>
-    </div>
+    )
 }
