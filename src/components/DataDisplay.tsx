f4713adc4aa13af4152ba1f7f4a5d1b2aea1316b
Brett Abraham <babraham341@gmail.com>
2025-11-16 12:12:38 -0500

    Connect to FakerAPI with useFaker hook

--- src/components/DataDisplay.tsx
+++ src/components/DataDisplay.tsx
@@ -1,4 +1,8 @@
-﻿export default function DataDisplay() {
+﻿import useFaker from "../hooks/useFaker.ts"
+
+export default function DataDisplay() {
+    useFaker('images', 3);
+
     return <div className="text-2xl">
         <p>Data Display</p>
     </div>
---
+++ src/hooks/useFaker.ts
@@ -0,0 +1,22 @@
+﻿import {useEffect} from "react";
+
+export default function useFaker(endpoint: string, quantity: number) {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const baseUrl: string = "https://fakerapi.it/api/v2";
+                const requestUrl: string = `${baseUrl}/${endpoint}?_quantity=${quantity}`
+
+                const response = await fetch(requestUrl);
+                const json = await response.json();
+                console.log(json)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+    }, [endpoint, quantity]);
+
+
+}
