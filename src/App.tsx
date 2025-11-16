d7c9a1f61b4d0d517c629209602dd71e8a06ca98
Brett Abraham <babraham341@gmail.com>
2025-11-16 12:11:41 -0500

    Setup DataDisplay component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import DataDisplay from "./components/DataDisplay.tsx";
 
 function App() {
     return (
         <div className="grid grid-cols-1">
             <div className="text-5xl font-bold">Hello World!</div>
+            <DataDisplay />
         </div>
     )
 }
---
+++ src/components/DataDisplay.tsx
@@ -0,0 +1,5 @@
+﻿export default function DataDisplay() {
+    return <div className="text-2xl">
+        <p>Data Display</p>
+    </div>
+}
