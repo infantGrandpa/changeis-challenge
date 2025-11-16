37c3e0d2d52a342a7bfdbd8ee8ad70db53cc8dfc
Brett Abraham <babraham341@gmail.com>
2025-11-16 11:37:50 -0500

    Format App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 
 function App() {
-  return (
-      <div className="grid grid-cols-1">
-        <div className="text-5xl font-bold">Hello World!</div>
-      </div>
-  )
+    return (
+        <div className="grid grid-cols-1">
+            <div className="text-5xl font-bold">Hello World!</div>
+        </div>
+    )
 }
 
 export default App
