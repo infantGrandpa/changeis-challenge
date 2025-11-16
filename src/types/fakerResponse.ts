cd8091ccbfd41f2a2a8efd7b5d3e5d0b18ac7480
Brett Abraham <babraham341@gmail.com>
2025-11-16 12:33:33 -0500

    Create response types

---
+++ src/types/fakerResponse.ts
@@ -0,0 +1,14 @@
+﻿export interface FakerResponse {
+    status: string;
+    code: number;
+    locale: string;
+    total: number;
+    seed?: null | string;
+    data: [ImageResponse]
+}
+
+export interface ImageResponse {
+    title: string;
+    description: string;
+    url: string;
+}
