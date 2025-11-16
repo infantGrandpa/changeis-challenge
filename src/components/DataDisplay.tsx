2ea957d60319d5914fe87ff51c113b6b62a032c6
Brett Abraham <babraham341@gmail.com>
2025-11-16 13:32:26 -0500

    Fetch and display multiple images at once

--- src/components/DataDisplay.tsx
+++ src/components/DataDisplay.tsx
@@ -2,15 +2,21 @@
 import type {ImageResponse} from "../types/fakerResponse.ts";
 
 export default function DataDisplay() {
-    const {data, error, isLoading} = useFaker('images', 1);
-    const image: ImageResponse | undefined = data?.data[0];
+    const {data, error, isLoading} = useFaker('images', 3);
 
     return <div className="text-2xl">
         <p>Data Display</p>
         {isLoading && <p>Loading...</p>}
         {error && <p className="text-red-500">Error: {error}</p>}
         <div className="flex flex-row flex-wrap">
-            {image && <img src={image.url} alt={image.title}/>}
+            {data?.map((image: ImageResponse, index: number) => (
+                <img
+                    key={index}
+                    src={image.url}
+                    alt={image.title}
+                    className={"w-48 h-48 object-cover"}
+                />
+            ))}
         </div>
     </div>
 }
